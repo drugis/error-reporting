e3ac1c3c8be32209576037012d7b148611ca46c0
Connor Stroomberg <c.stroomberg@gmail.com>
2016-03-17 15:23:12 +0100

    error reporting for gemtc

--- errorReportingDirective.js
+++ errorReportingDirective.js
@@ -1,27 +1,22 @@
+'use strict';
+/* globals angular */
 angular.module('errorReporting', [])
 
 .factory('ErrorReportingService', ['$rootScope', '$q', function($rootScope, $q) {
 
   function handleReaction(rejection) {
-    var message;
-    if (rejection && rejection.status === 404) {
-      $rootScope.$broadcast('error', {
-        code: 404,
-        cause: rejection.data.error
-      });
-      return $q.reject(rejection);
-    } else if (rejection && rejection.data && rejection.data !== "") {
-      message = {
-        code: rejection.data.code,
-        cause: rejection.data.message
-      };
-    } else {
-      message = {
-        cause: 'An unknown error occurred'
+    var errorHolder = {
+      type: 'BACK_END_ERROR',
+      cause: 'An unknown error occurred' // defualt message
+    };
+    if (rejection && rejection.status, rejection.data) {
+      errorHolder = {
+        code: rejection.status,
+        cause: rejection.data
       };
     }
 
-    $rootScope.$broadcast('error', message);
+    $rootScope.$broadcast('error', errorHolder);
     return $q.reject(rejection);
   }
 
@@ -34,17 +29,17 @@ angular.module('errorReporting', [])
     }
   };
 }])
-.directive('errorReporting', ['$rootScope','ErrorReportingService', function($rootScope, ErrorReportingService) {
+.directive('errorReporting', ['$rootScope', '$window','ErrorReportingService', function($rootScope, $window) {
   return {
     restrict: 'E',
     template: '' +
     '<div ng-if="error" class="row" ng-cloak>' +
-      '<div class="columns">' +
-      '<div class="alert-box {{type}}">' +
-        '<div class="alert-box-message">' +
-      	  '<a ng-click="animatedClose()" class="close" style="top: 1rem;">&times;</a>' +
+      '<div class="columns large-10 medium-10 small-12 medium-centered large-centered ">' +
+      '<div class="alert-box alert">' +
+        '<div class="alert-box-message" style="font-size: 1.25rem">' +
+      	  '<a ng-click="closeError()" class="close" style="top: 1.25rem; font-size: 2rem">&times;</a>' +
       	  '<div ng-if="error.type !== \'patavi\'">' +
-      		  '{{error.code}} {{error.cause}} {{error.message}}' +
+      		  '<div>{{error.code}}</div> <div>{{error.cause}}</div> <div>{{error.message}}</div>' +
           '</div>' +
           '<div ng-if="error.type === \'patavi\'">' +
       		  '<p>An error has occured while running the model in R.</p>' +
@@ -54,17 +49,18 @@ angular.module('errorReporting', [])
        '</div>' +
       '</div>' +
     '</div>',
-    link: function() {
+    link: function(scope, element) {
       $rootScope.$on('error', function(e, error) {
-        $rootScope.error = _.extend(error, {
-          close: function() {
-            delete $rootScope.error;
-          }
-        });
-      })
+        $rootScope.error = error;
+        // todo need to scroll to top
+      });
+
+      $rootScope.closeError = function() {
+        delete $rootScope.error;
+      };
     }
-  }
+  };
 }])
 .config(['$httpProvider', function($httpProvider) {
-  $httpProvider.interceptors.push('errorInterceptor');
+  $httpProvider.interceptors.push('ErrorReportingService');
 }]);
