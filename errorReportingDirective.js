cffc646fd7a27d3915cee0441cfdaa14511fb4c7
Connor Stroomberg <c.stroomberg@gmail.com>
2016-06-16 15:02:53 +0200

    update template

--- errorReportingDirective.js
+++ errorReportingDirective.js
@@ -51,12 +51,15 @@ angular.module('errorReporting', [])
           '<div class="alert-box alert">' +
           '<div class="alert-box-message" style="font-size: 1.25rem">' +
           '<a ng-click="closeError()" class="close" style="top: 1.25rem; font-size: 2rem">&times;</a>' +
-          '<div ng-if="error.type !== \'patavi\'">' +
-          '<div>{{error.code}}</div> <div>{{error.cause}}</div> <div>{{error.message}}</div>' +
+          '<div ng-if="error.type !== \'PATAVI\'">' +
+          '  <div ng-if="error.code>{{::error.code}}</div> ' +
+          '  <div ng-if="error.cause>{{::error.cause}}</div> ' +
+          '  <div>{{::error.message}}</div>' +
           '</div>' +
-          '<div ng-if="error.type === \'patavi\'">' +
-          '<p>An error has occured while running the model in R.</p>' +
-          '<div>{{error.cause}}</div> <div>{{error.message}}</div>' +
+          '<div ng-if="error.type === \'PATAVI\'">' +
+          '  <p>An error has occured while running the model in R.</p>' +
+          '  <div ng-if="error.cause>{{::error.cause}}</div>' +
+          '  <div ng-if="error.message>{{::error.message}}</div>' +
           '</div>' +
           '</div>' +
           '</div>' +
