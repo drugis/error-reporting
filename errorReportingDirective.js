dc08775e674de116d86fc995321b1df3b1c2bc54
Joris de Keijser <chinook25@gmail.com>
2017-09-28 11:34:39 +0200

    update for ui-router 1 and foundation 6

--- errorReportingDirective.js
+++ errorReportingDirective.js
@@ -2,72 +2,67 @@
 /* globals angular */
 angular.module('errorReporting', [])
 
-.factory('ErrorReportingService', ['$rootScope', '$q',
-  function($rootScope, $q) {
+  .factory('ErrorReportingService', ['$rootScope', '$q',
+    function($rootScope, $q) {
 
-    function handleReaction(rejection) {
+      function handleReaction(rejection) {
 
-      var errorHolder = {
-        type: 'BACK_END_ERROR',
-        cause: 'An unknown error occurred' // default message
-      };
-      if (rejection && rejection.status && (rejection.statusText || rejection.data)) {
-        errorHolder = {
-          code: rejection.status,
-          cause: rejection.statusText || rejection.data
+        var errorHolder = {
+          type: 'BACK_END_ERROR',
+          cause: 'An unknown error occurred' // default message
         };
+        if (rejection && rejection.status && (rejection.statusText || rejection.data)) {
+          errorHolder = {
+            code: rejection.status,
+            cause: rejection.statusText || rejection.data
+          };
+        }
+
+        $rootScope.$broadcast('error', errorHolder);
+        return $q.reject(rejection);
       }
 
-      $rootScope.$broadcast('error', errorHolder);
-      return $q.reject(rejection);
+      return {
+        'requestError': function(rejection) {
+          return handleReaction(rejection);
+        },
+        'responseError': function(rejection) {
+          return handleReaction(rejection);
+        }
+      };
     }
-
-    return {
-      'requestError': function(rejection) {
-        return handleReaction(rejection);
-      },
-      'responseError': function(rejection) {
-        return handleReaction(rejection);
-      }
-    };
-  }
-])
-  .directive('errorReporting', ['$rootScope', '$window', 'ErrorReportingService',
-    function($rootScope, $window) {
+  ])
+  .directive('errorReporting', ['$rootScope', '$transitions', '$window',
+    function($rootScope, $transitions, $window) {
       return {
         restrict: 'E',
         template: '' +
-          '<div ng-if="error" class="row" ng-cloak>' +
-          '<div class="columns large-10 medium-10 small-12 medium-centered large-centered"  style="margin: 1rem;">' +
-          '<div class="alert-box alert">' +
-          '<div class="alert-box-message" style="font-size: 1.25rem">' +
-          '<a ng-click="closeError()" class="close" style="top: 1.25rem; font-size: 2rem">&times;</a>' +
-          '<div ng-if="error.type !== \'PATAVI\'">' +
-          '  <div ng-if="error.code">{{::error.code}}</div> ' +
-          '  <div ng-if="error.cause">{{::error.cause}}</div> ' +
-          '  <div>{{::error.message}}</div>' +
-          '</div>' +
-          '<div ng-if="error.type === \'PATAVI\'">' +
-          '  <p>An error has occured while running the model in R.</p>' +
-          '  <div ng-if="error.cause">{{::error.cause}}</div>' +
-          '  <div ng-if="error.message">{{::error.message}}</div>' +
-          '</div>' +
-          '</div>' +
-          '</div>' +
-          '</div>' +
+          '<div ng-if="error" class="grid-x" ng-cloak>' +
+          ' <div class="cell large-10 medium-10 small-12 medium-centered large-centered"  style="margin: 1rem;">' +
+          '   <alert type="\'alert\'" close="closeError()">' +
+          '     <div ng-if="error.type !== \'PATAVI\'">' +
+          '       <div ng-if="error.code">{{::error.code}}</div> ' +
+          '       <div ng-if="error.cause">{{::error.cause}}</div> ' +
+          '       <div>{{::error.message}}</div>' +
+          '     </div>' +
+          '     <div ng-if="error.type === \'PATAVI\'">' +
+          '       <p>An error has occured while running the model in R.</p>' +
+          '       <div ng-if="error.cause">{{::error.cause}}</div>' +
+          '       <div ng-if="error.message">{{::error.message}}</div>' +
+          '     </div>' +
+          '   </alert>' +
+          ' </div>' +
           '</div>',
         link: function(scope, element) {
           $rootScope.$on('error', function(e, error) {
             $rootScope.error = error;
-            // todo need to scroll to top
+            $window.scrollTo(0, 0);
           });
 
-          $rootScope.$on("$stateChangeSuccess",
-            function(event, toState) {
-              $rootScope.currentStateName = toState.name;
-              delete $rootScope.error;
-            }
-          );
+          $transitions.onStart({}, function(transition) {
+            $rootScope.currentStateName = transition.to();
+            delete $rootScope.error;
+          });
 
           $rootScope.closeError = function() {
             delete $rootScope.error;
